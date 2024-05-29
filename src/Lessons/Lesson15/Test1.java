0b8ecd6edec79749d6f4d33f21880754e0164241
Serkebaeva <aizada.serkebaeva.lt@gmail.com>
2024-05-29 18:06:03 +0200

    Lesson 15 done

---
+++ src/Lessons/Lesson15/Test1.java
@@ -0,0 +1,23 @@
+package Lessons.Lesson15;
+
+public class Test1 {
+    public static void main(String[] args) {
+        int a = 5;
+        while (a++ < 10) {
+            a++;
+
+        }
+
+        System.out.println(a);
+
+        int b = 5;
+        while (++b < 10) {
+            b++;
+
+        }
+
+        System.out.println(b);
+
+    }
+
+}
